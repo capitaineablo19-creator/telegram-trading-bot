b15b4fc985e03f6c3e54a5c70dbfc730e6e60968
capitaineablo19-creator <capitaineablo19@gmail.com>
2026-05-12 18:15:51 +0000

    Create signals.py

---
+++ signals.py
@@ -0,0 +1,13 @@
+import requests
+from config import BOT_TOKEN, CHAT_ID
+
+def send_signal(message):
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    requests.post(url, data=data)
